24bd6e3a3b97d44721fa79e820ca0f1232781c30
Tido <99051567@mydavinci.nl>
2021-09-08 11:33:37 +0200

    Enabled Automatic migrations
    I also made sure the file structure matches that of the local version

--- Jukebox/Jukebox/Jukebox/Migrations/Configuration.cs
+++ Jukebox/Jukebox/Jukebox/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace Jukebox.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(Jukebox.Model1 context)
